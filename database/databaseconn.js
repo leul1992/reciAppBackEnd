5aa055de2f5692bc1543872b3132b33ad7ea74aa
Leulseged Bekele <88849889+Leulbkm@users.noreply.github.com>
2023-10-19 12:24:06 +0300

    fix: postgre url

--- database/databaseconn.js
+++ database/databaseconn.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  connectionString: 'postgres://leul:qxYPhvhkMqKpMl2CbNwOuVObAeWvjGAy@dpg-ckjuloolk5ic738cavlg-a/reciapp_hs05',
+  connectionString: 'postgres://leul:qxYPhvhkMqKpMl2CbNwOuVObAeWvjGAy@dpg-ckjuloolk5ic738cavlg-a.singapore-postgres.render.com/reciapp_hs05',
   ssl: {
     rejectUnauthorized: true
   }
