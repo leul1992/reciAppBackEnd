eaa1f0f7c699fadd141450fa7f41a9159709e851
Leulseged Bekele <88849889+Leulbkm@users.noreply.github.com>
2023-10-20 10:53:31 +0300

    fix: roll back

--- server/index.js
+++ server/index.js
@@ -1,30 +1,8 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
-const cors = require('cors'); // Import the cors package
 
 app.use(express.json());
-
-// Define a whitelist of allowed origins (including your Vercel app and Render.com static IPs)
-const whitelist = [
-  'http://localhost:3000', // Your local development server
-  'https://reci-app-front-end.vercel.app', // Your Vercel-hosted React app
-  'http://13.228.225.19', // Render.com static IP
-  'http://18.142.128.26',  // Render.com static IP
-  'http://54.254.162.138'  // Render.com static IP
-];
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-};
-
-app.use(cors(corsOptions)); // Use the cors middleware with the specified options
 
 // Import route files
 const signupRoute = require('./routes/signup');
