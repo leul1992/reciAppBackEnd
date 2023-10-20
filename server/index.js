ae0bcb5664da847b191be9902b49696832acaa73
Leulseged Bekele <88849889+Leulbkm@users.noreply.github.com>
2023-10-20 11:47:24 +0300

    fix: config CORS

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const cors = require("cors"); // Import the cors middleware
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
@@ -17,6 +18,12 @@ app.use(loginRoute);
 app.use(saveFavouritesRoute);
 app.use(showFavouritesRoute);
 app.use(deleteFromfavouritesRoute);
+
+// Enable CORS with specific origins
+app.use(cors({
+  origin: ['https://reci-app-front-lukd8953v-leul1992.vercel.app', 'localhost:3000'], // Replace with your frontend's domain
+  optionsSuccessStatus: 200, // Some legacy browsers (IE11) choke on a 204 response.
+}));
 
 // Start the server
 app.listen(PORT, () => {
